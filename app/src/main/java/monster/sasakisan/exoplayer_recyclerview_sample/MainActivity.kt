a6967f38ed4f5390b2b2bf402ce157941db5b8f1
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2019-06-23 21:44:18 +0900

    first commit

---
+++ app/src/main/java/monster/sasakisan/exoplayer_recyclerview_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package monster.sasakisan.exoplayer_recyclerview_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
